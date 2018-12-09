d60c1c3470b70546c59a11579aef33fc07042ce0
Paul Orlob <orlobpaul@gmail.com>
2018-12-09 12:45:37 +0100

    Added word length to dtypes

--- lib/modbus_typings.ts
+++ lib/modbus_typings.ts
@@ -12,6 +12,24 @@ export enum ModbusDatatype {
     uint32,
     uint64
 }
+export namespace ModbusDatatype {
+    export function words(dtype: ModbusDatatype): number | undefined{
+        switch (dtype) {
+            case ModbusDatatype.acc32: return 2;
+            case ModbusDatatype.acc64: return 4;
+            case ModbusDatatype.bitfield16: return 1;
+            case ModbusDatatype.bitfield32: return 2;
+            case ModbusDatatype.enum16: return 1;
+            case ModbusDatatype.int16: return 1;
+            case ModbusDatatype.int32: return 2;
+            case ModbusDatatype.string: return undefined;
+            case ModbusDatatype.sunssf: return undefined;
+            case ModbusDatatype.uint16: return 1;
+            case ModbusDatatype.uint32: return 2;
+            case ModbusDatatype.uint64: return 4;
+        }
+    }
+}
 
 export enum ModbusPermission {
     RW,
