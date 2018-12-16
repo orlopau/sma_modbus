e0493a5dbc27701c51d1cc19dca95c6a162ff6c3
Paul Orlob <orlobpaul@gmail.com>
2018-12-16 01:23:28 +0100

    Added index file

---
+++ index.ts
@@ -0,0 +1,11 @@
+import {SunnyBoy} from "./lib/devices/sunny_boy";
+import {SunnyIsland} from "./lib/devices/sunny_island";
+import {BasicSMADevice} from "./lib/sma_device";
+
+export default {
+    Devices: {
+        SunnyBoy,
+        SunnyIsland,
+        BasicSMADevice
+    }
+}
