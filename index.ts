3732d13c689f4fe47cfa6a2f1c3c3e5a620f7cdd
Paul Orlob <orlobpaul@gmail.com>
2018-12-19 13:16:21 +0100

    Added mdbus dt to export

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export {SunnyBoy} from "./lib/devices/sunny_boy";
 export {SunnyIsland} from "./lib/devices/sunny_island";
 export {BasicSMADevice} from "./lib/sma_device";
+export {ModbusDatatype} from "./lib/modbus_typings"
